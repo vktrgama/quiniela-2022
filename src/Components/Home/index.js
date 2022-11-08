52cd95a418af3e06ecdb537c8c8ebf9762ed0885
Victor Gama <vktrgama@gmail.com>
2022-11-08 15:14:00 -0800

    add paypal button and rules for vktr-quiniela

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -8,6 +8,8 @@ import {
     Avatar,
     Typography,
   } from '@mui/material';
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { useApp } from '../../contexts/App'
 import './home.css';
@@ -23,11 +25,24 @@ const Home = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
+    const handlePaypal = () => {
+        window.location = "https://www.paypal.com/donate/?hosted_button_id=FM3NTSGGXKBWJ";
+    }
+
     return <div className='home'>
         <div className="hero">
             <Stack direction='row' spacing={2} justifyContent='center' alignItems='center' divider={<Divider orientation='vertical' flexItem />}>
                 <div className='container-left'>
                     <h1>Welcome to Quiniela Qatar 2022</h1>
+                    <div id="donate-button-container">
+                        <div id="donate-button">
+                            <p>(Para espaniol, use google translate, en la esquina inferior derecha).</p>
+                            <p>Just to make it interesting, your participation required a donation of $10.00, using the button below. FYI, Paypal takes a 3% (*) comission from it, so winner will get the rest (accumalted $7.00 from each participant), people with no donation will be removed when the turnament starts.</p>
+                            <p>Winner takes all, no second or third place. Winner is the person with the high number of points, if a draw happen, person with more exact results wins.</p>
+                            <Button variant="contained" endIcon={<SendIcon />} onClick={handlePaypal}>Donate</Button>
+                            <p><a href="https://www.paypal.com/webapps/mpp/merchant-fees">PayPal comission details <b>here</b></a></p>
+                        </div>
+                    </div>
                 </div>
                 <div className='container-right'>
                 <Stack
