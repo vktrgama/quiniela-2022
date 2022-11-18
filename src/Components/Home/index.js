1071f553adba5963b65eef4502914790127250b5
Victor Gama <vktrgama@gmail.com>
2022-11-18 12:02:59 -0800

    scroll participants in homepage

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -38,44 +38,46 @@ const Home = () => {
                     justifyContent="flex-start"
                     alignItems="flex-end"
                     >
-                    <span>List of participants:</span>
-                    <List dense sx={{ width: '300px', maxWidth: 360 }}>
-                        {users.map((p, idx) => (
-                            <ListItem key={idx}>
-                                <ListItemAvatar>
-                                <Avatar>
-                                    <AccountCircleIcon />
-                                </Avatar>
-                                </ListItemAvatar>
-                                <ListItemText 
-                                    primary={
-                                        <React.Fragment>
-                                            <Typography
-                                                sx={{ display: 'inline', fontSize: '18px' }}
-                                                component="span"
-                                                variant="body2"
-                                                color="white"
-                                            >
-                                                {p.name}
-                                            </Typography>
-                                        </React.Fragment>
-                                    }
-                                    secondary={
-                                        <React.Fragment>
-                                            <Typography
-                                                sx={{ display: 'inline' }}
-                                                component="span"
-                                                variant="body2"
-                                                color="white"
-                                            >
-                                                {p.totalPoints} points
-                                            </Typography>
-                                        </React.Fragment>
-                                    }
-                                />
-                            </ListItem>
-                        ))} 
-                    </List>
+                    <div className='participants-list'>
+                        <span className='participants-title'>List of participants ({users.length}):</span>
+                        <List dense sx={{ width: '300px', maxWidth: 360 }}>
+                            {users.map((p, idx) => (
+                                <ListItem key={idx}>
+                                    <ListItemAvatar>
+                                    <Avatar>
+                                        <AccountCircleIcon />
+                                    </Avatar>
+                                    </ListItemAvatar>
+                                    <ListItemText 
+                                        primary={
+                                            <React.Fragment>
+                                                <Typography
+                                                    sx={{ display: 'inline', fontSize: '18px' }}
+                                                    component="span"
+                                                    variant="body2"
+                                                    color="white"
+                                                >
+                                                    {p.name}
+                                                </Typography>
+                                            </React.Fragment>
+                                        }
+                                        secondary={
+                                            <React.Fragment>
+                                                <Typography
+                                                    sx={{ display: 'inline' }}
+                                                    component="span"
+                                                    variant="body2"
+                                                    color="white"
+                                                >
+                                                    {p.totalPoints} points
+                                                </Typography>
+                                            </React.Fragment>
+                                        }
+                                    />
+                                </ListItem>
+                            ))} 
+                        </List>
+                    </div>
                 </Stack>
                 </div>
             </Stack>
