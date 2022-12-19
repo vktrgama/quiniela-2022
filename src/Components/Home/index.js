780af0a7f062eedeac1004a1729f0c3aa88279dd
Victor Gama <vktrgama@gmail.com>
2022-12-19 10:59:50 -0800

    move donations to rules

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -8,8 +8,6 @@ import {
     Avatar,
     Typography,
   } from '@mui/material';
-import Button from '@mui/material/Button';
-import SendIcon from '@mui/icons-material/Send';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { useApp } from '../../contexts/App'
 import './home.css';
@@ -25,10 +23,6 @@ const Home = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
-    const handlePaypal = () => {
-        window.location = "https://www.paypal.com/donate/?hosted_button_id=XXXXX";
-    }
-
     return <div className='home'>
         <div className="hero">
             <Stack direction='row' spacing={2} justifyContent='center' alignItems='center' divider={<Divider orientation='vertical' flexItem />}>
@@ -36,14 +30,7 @@ const Home = () => {
                     <h1>Welcome to Quiniela Qatar 2022</h1>
                     <div id="donate-button-container">
                         <h1 className='congrats'>Congratulations <span>{users && users.length ? users[0].name : ''}!</span> <span>you are the winner!</span></h1>
-                        <div id="donate-button">
-                            <p><b>(Para espaniol, usa el traductor de google que esta en la esquina inferior derecha)</b>.</p>
-                            <p>To make it interesting, your participation requires a donation of $XX.00, using the button below. The winner will get the rest (accumulated per participant), people with no donation will be removed when the turnament starts.</p>
-                            <p>Winner takes all, no second or third place. Winner is the person with the higher number of points, if a draw happes, the person with more exact results with in the elimination brackets wins.</p>
-                            <p><b>To start click [User Matches] from the menu above.</b></p>
-                            <Button variant="contained" endIcon={<SendIcon />} onClick={handlePaypal}>Donate</Button>
-                            <p><a href="https://www.paypal.com/webapps/mpp/merchant-fees" target="_new">PayPal comission details <b>here</b></a> under <i>Receiving and Sending Donations</i></p>
-                        </div>
+                        <p><b>(Para espaniol, usa el traductor de google que esta en la esquina inferior derecha)</b>.</p>
                     </div>
                 </div>
                 <div className='container-right'>
